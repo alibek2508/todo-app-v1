945266fac681ea33844b27dc3edfb2accfa34c5a
Alibek Tukhbayev <a.tukhbayev@yandex.ru>
2024-04-20 10:03:56 +0500

    merg

--- src/components/Todos/TodoForm.jsx
+++ src/components/Todos/TodoForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import styles from "./TodoForm.module.css";
 function TodoForm({ addTodo }) {
 	const [text, setText] = useState("");
 	const onSubmitHandler = e => {
@@ -8,15 +8,17 @@ function TodoForm({ addTodo }) {
 		setText("");
 	};
 	return (
-		<form onSubmit={onSubmitHandler}>
-			<input
-				type="text"
-				placeholder="Enter new todo"
-				value={text}
-				onChange={e => setText(e.target.value)}
-			/>
-			<button type="submit">Submit</button>
-		</form>
+		<div className={styles.todoFormContainer}>
+			<form onSubmit={onSubmitHandler}>
+				<input
+					type="text"
+					placeholder="Enter new todo"
+					value={text}
+					onChange={e => setText(e.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
 	);
 }
 
